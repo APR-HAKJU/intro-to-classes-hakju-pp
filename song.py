f19eb2204498df5ec57d4f10d1f9d560b2daf318
Rehcap <paul59.pacher@icloud.com>
2025-11-25 11:24:05 +0100

    Added songs

--- song.py
+++ song.py
@@ -2,9 +2,15 @@
 # Verändere den Code so, dass eine Klasse "Song" definiert wird.
 # Die Eigenschaften Titel, Künstler und Anzahl der Streams sollen als Attribute im Konstruktor definiert werden
 
-song_1 = ["Blinding Lights", "The Weeknd", 4_200_000_000]
-song_2 = ["Shape of You", "Ed Sheeran", 5_300_000_000]
-song_3 = ["Dance Monkey", "Tones and I", 3_400_000_000]
+class Song:
+    def __init__(self, titel, Künstler, Anzahl_Streams):
+        self.titel = titel
+        self.künstler = Künstler
+        self.anzahl_streams = int(Anzahl_Streams)
+        
+song_1 = Song("Blinding Lights", "The Weeknd", 4_200_000_000)
+song_2 = Song("Shape of You", "Ed Sheeran", 5_300_000_000)
+song_3 = Song("Dance Monkey", "Tones and I", 3_400_000_000)
 
 #TODO: Aufgabe 1: 
 #   Definiere die Klasse "Song" mit einem Konstruktor (__init__),
@@ -16,3 +22,7 @@ song_3 = ["Dance Monkey", "Tones and I", 3_400_000_000]
 # TODO: Aufgabe 3:
 #   Gib für jeden Song den Titel und die Anzahl der Streams in folgendem Format aus:
 #   "Der Song '<Titel>' von <Künstler> hat <Anzahl der Streams> Streams."
+
+print(f"Der Song {song_1.titel} von {song_1.künstler} hat {song_1.anzahl_streams} Streams.")
+print(f"Der Song {song_2.titel} von {song_2.künstler} hat {song_2.anzahl_streams} Streams.")
+print(f"Der Song {song_3.titel} von {song_3.künstler} hat {song_3.anzahl_streams} Streams.")
